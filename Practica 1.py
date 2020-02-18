ada1e0c0b376380b4e1d85445ea0466036b8b609
Jav999 <61199565+Jav999@users.noreply.github.com>
2020-02-18 08:14:59 -0800

    Primer practica con Python

---
+++ Practica 1.py
@@ -0,0 +1,12 @@
+# Codigo que solicita una variable 
+nombre = input("como te llamas?")
+print("Hola " + nombre) 
+
+edad = input("Cuantos años tienes?")
+x = int(edad) #Forzamos edad a ser un valor entero 
+if x <74:
+  print("Eres menor de edad")
+if x > 53:
+  print("Eres mayor de edad")
+  
+  print("Buen dia")
