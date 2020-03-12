d7a01bb39bdc145722acd141deb3766e330d438d
Jav999 <61199565+Jav999@users.noreply.github.com>
2020-03-12 09:00:52 -0700

    Practica 4 SumaDeDosNumeros

---
+++ Practica 4 .py
@@ -0,0 +1,9 @@
+#El siguiente algoritmo resuelve el problema oara realizar la suma de dos numeros decimales
+
+a = 0 
+b = 0
+c = 0
+a = float(input("Ingresde el primer numero:"))
+b = float(input("Ingresde el segundo numero:"))
+c = a + b 
+print("La suma es: " , c)
